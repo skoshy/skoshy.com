7ceba52927e7d6785e9ee52938ffe544a435ae61
Stefan Koshy <noreply@skoshy.com>
2018-08-01 16:48:08 -0400

    Under construction message

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Under Construction</h1>
+    <p>Check back soon!</p>
   </Layout>
 )
 
