2536a873b4eacf3c9ccc78238477ed454bad640b
Stefan Koshy <noreply@skoshy.com>
2018-08-13 16:02:18 -0400

    Added some basic stuff to the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,17 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Under Construction</h1>
-    <p>Check back soon!</p>
+    <div className="alert alert-warning">
+      <h2>Under Construction</h2>
+      <p>Pardon the appearance, this site is still under construction!</p>
+    </div>
+    <h1>Howdy</h1>
+    <h3>I&apos;m <abbr title="Pronounced STEHP-on">Stefan</abbr> (or Steve) Koshy</h3>
+    <p>
+      I&apos;m a <a href="https://nextjump.com" rel="noopener noreferrer" target="_blank">Next Jumper</a> currently residing in New York City
+    </p>
+    <h3>Web Development is my passion</h3>
+    <p>Full Stack Javascript with React or Vue preferred, but experienced in PHP and Golang as well</p>
   </Layout>
 )
 
