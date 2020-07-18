e1f66a03b6a819d2784615fbe793c15c355eae0b
Stefan Koshy <noreply@skoshy.com>
2020-07-18 01:28:13 -0400

    Upgrade deps, remove unneeded ones

--- src/components/base/Link.tsx
+++ src/components/base/Link.tsx
@@ -1,4 +1,4 @@
-import React, { AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
+import React, { AnchorHTMLAttributes, DetailedHTMLProps, useMemo } from 'react';
 import { css } from 'styled-components';
 import NextLink from 'next/link';
 import { FC } from 'src/utils/types';
@@ -22,11 +22,17 @@ export const Link: FC<Types> = ({ children, href, ...props }) => {
     `,
   ];
 
-  return (
-    <NextLink href={href}>
+  const anchor = useMemo(
+    () => (
       <a href={href} css={[style]} {...props}>
         {children}
       </a>
-    </NextLink>
+    ),
+    []
   );
+
+  const isExternalLink =
+    href.startsWith('http://') || href.startsWith('https://');
+
+  return isExternalLink ? anchor : <NextLink href={href}>{anchor}</NextLink>;
 };
--- src/utils/types.tsx
+++ src/utils/types.tsx
@@ -1,11 +1,4 @@
 /* eslint-disable no-restricted-imports, @typescript-eslint/no-explicit-any */
 import { FunctionComponent } from 'react';
-import PT from 'prop-types';
-import UT from 'utility-types';
-
-export type InferPropsTypes<PT extends object, DP extends object> = UT.Assign<
-  Partial<UT.Intersection<DP, PT>>,
-  Required<PT.InferProps<UT.Diff<PT, DP>>>
->;
 
 export type FC<PropsTypes = {}> = FunctionComponent<PropsTypes>;
