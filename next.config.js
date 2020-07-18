2ba55b7de92cc0243025385483ac822a828367ae
Stefan Koshy <noreply@skoshy.com>
2020-07-18 01:15:17 -0400

    Add tailwind

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const withSass = require('@zeit/next-sass');
 const {
   PHASE_DEVELOPMENT_SERVER,
   PHASE_PRODUCTION_BUILD,
@@ -10,15 +9,14 @@ const {
 module.exports = phase => {
   const ENV = phase === PHASE_DEVELOPMENT_SERVER ? 'development' : 'production'; // when `next build` or `npm run build` is used
 
-  return withSass({
+  return {
     env: {
       ENV,
       SITE_TITLE: 'Stefan Koshy',
     },
-    cssModules: true,
     webpack(config) {
       config.resolve.modules.push(path.resolve('./'));
       return config;
     },
-  });
+  };
 };
