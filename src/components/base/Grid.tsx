05180ec0fb17e4ca4a721131383e846809bbb4cd
Stefan Koshy <noreply@skoshy.com>
2021-02-11 15:50:31 -0500

    Fix for grid

--- src/components/base/Grid.tsx
+++ src/components/base/Grid.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid as GrommetGrid } from 'grommet';
 
-export const Grid = ({ className, children, ...props }) => (
+export const Grid = ({ className = '', children, ...props }) => (
   <div className={`grid ${className}`} {...props}>
     {children}
   </div>
